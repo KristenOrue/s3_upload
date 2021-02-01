348c27b255aa6c95cd414a5fe63ecf852e0edc39
KristenOrue <oruek@oregonstate.edu>
2021-01-31 16:48:57 -0800

    User can now upload files to s3 bucket after assuming the role

--- app.rb
+++ app.rb
@@ -1,63 +1,67 @@
 require 'aws-sdk'
 require 'aws-sdk-s3'
 
-# Uploads an object to a bucket in Amazon Simple Storage Service (Amazon S3).
-  def run_me
-    #assume the role
-    role_credentials = Aws::AssumeRoleCredentials.new(
-      client: Aws::STS::Client.new,
-      role_arn: "arn:aws:iam::589772831734:role/role4dan",
-      role_session_name: "s3-upload-session"
-    )
+#Command Line argument work
+NO_SUCH_BUCKET = "The bucket '%s' does not exist!"
 
-    bucket_name = 'meusick-bucket'
-    object_key = 'Scream.mp3'
-    region = 'us-west-2'
-    s3_client = Aws::S3::Client.new(credentials: role_credentials)
+USAGE = <<DOC
 
-    if object_uploaded?(s3_client, bucket_name, object_key)
-      puts "Object '#{object_key}' uploaded to bucket '#{bucket_name}'."
-    else
-      puts "Object '#{object_key}' not uploaded to bucket '#{bucket_name}'."
-    end
-  end
+Usage: hello-s3 bucket_name [operation] [file_name]
 
-  def object_uploaded?(s3_client, bucket_name, object_key)
-    response = s3_client.put_object(
-      bucket: bucket_name,
-      key: object_key
-    )
-    if response.etag
-      return true
-    else
-      return false
-    end
-  rescue StandardError => e
-    puts "Error uploading object: #{e.message}"
-    return false
-  end
+Where:
+  bucket_name (required) is the name of the bucket
 
-  run_me if $PROGRAM_NAME == __FILE__
+  operation   is the operation to perform on the bucket:
+              create  - creates a new bucket
+              upload  - uploads a file to the bucket
+              list    - (default) lists up to 50 bucket items
 
+  file_name   is the name of the file to upload,
+              required when operation is 'upload'
 
+DOC
+
+#Sets the name of the bucket on which the operations are performed
+bucket_name = nil
+
+if ARGV.length > 0
+  bucket_name = ARGV[0]
+else
+  puts USAGE
+  exit 1
+end
+
+#The operation to be performed on the bucket
+operation = ARGV[1] if (ARGV.length > 1)
+
+#The file name to use alongside 'upload'
+file = nil
+file = ARGV[2] if (ARGV.length > 2)
 
 #assume the role
-# role_credentials = Aws::AssumeRoleCredentials.new(
-#   client: Aws::STS::Client.new,
-#   role_arn: "arn:aws:iam::589772831734:role/role4dan",
-#   role_session_name: "s3-upload-session"
-# )
+role_credentials = Aws::AssumeRoleCredentials.new(
+  client: Aws::STS::Client.new,
+  role_arn: "arn:aws:iam::589772831734:role/role4dan",
+  role_session_name: "s3-upload-session"
+)
 
-# s3_client = Aws::S3::Client.new(credentials: role_credentials)
+#Get the Amazon client role credentials
+s3_client = Aws::S3::Client.new(credentials: role_credentials)
 
-# #list s3 Objects
-# # test should fail until out role is assumed
-# pp s3_client.list_buckets
-
-# #upload a file
-# s3_client.put_object()
-
-# # upload file from disk in a single request, may not exceed 5GB
-# File.open('/source/file/path', 'rb') do |file|
-#   s3_client.put_object(bucket: 'bucket-name', key: 'object-key', body: file)
-# end
+#Different the operation name matches ARGV[1]
+case operation
+when 'upload'
+  if file == nil
+    puts "You must enter a file name to upload to S3!"
+    exit
+  else 
+    file_name= File.basename file
+    # puts "File has been stored: '%s'" %file_name
+    # puts "Bucket has been stored: '%s'" %bucket_name
+    s3_client.put_object( bucket: bucket_name, key: file_name)
+    puts "File '#{file_name}' uploaded to bucket '#{bucket_name}'."
+  end
+else
+  puts "Unknown operation: '%s'!" % operation
+  puts USAGE
+end
