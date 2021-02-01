97fd98b889700c537621dcacb10148b94a077a94
KristenOrue <oruek@oregonstate.edu>
2021-01-31 20:33:48 -0800

    Added instructions for how to run program

--- app.rb
+++ app.rb
@@ -12,20 +12,34 @@ NO_SUCH_BUCKET = "The bucket '%s' does not exist!"
 
 USAGE = <<DOC
 
-Usage: hello-s3 bucket_name [operation] [file_name]
+Usage: ruby app.rb [bucket_name] [operation] [file_name]
 
 Where:
-  bucket_name (required) is the name of the bucket
 
-  operation   is the operation to perform on the bucket:
-              create  - creates a new bucket
-              upload  - uploads a file to the bucket
-              list    - (default) lists up to 50 bucket items
+bucket_name (required) is the name of the bucket
 
-  file_name   is the name of the file to upload,
-              required when operation is 'upload'
+operation   is the operation to perform on the bucket:
+            upload  - uploads a file to the bucket
+            list    - lists bucket objects in a particular s3 bucket
+            rename  - Allows you to rename a file in an s3 bucket
+
+file_name   is the name of the file to upload, which can be a File path or a filename
+            required when operation is 'upload'
+
+Buckets are listed below: 
 
 DOC
+
+
+#assume the role
+role_credentials = Aws::AssumeRoleCredentials.new(
+  client: Aws::STS::Client.new,
+  role_arn: "arn:aws:iam::589772831734:role/role4dan",
+  role_session_name: "s3-upload-session"
+)
+
+#Get the Amazon client role credentials
+s3_client = Aws::S3::Client.new(credentials: role_credentials)
 
 #Sets the name of the bucket on which the operations are performed
 bucket_name = nil
@@ -34,6 +48,7 @@ if ARGV.length > 0
   bucket_name = ARGV[0]
 else
   puts USAGE
+  pp s3_client.list_buckets
   exit 1
 end
 
@@ -47,16 +62,6 @@ file = ARGV[2] if (ARGV.length > 2)
 #The new name to use alongside 'rename'
 new_name = nil
 new_name = ARGV[3] if (ARGV.length > 3)
-
-#assume the role
-role_credentials = Aws::AssumeRoleCredentials.new(
-  client: Aws::STS::Client.new,
-  role_arn: "arn:aws:iam::589772831734:role/role4dan",
-  role_session_name: "s3-upload-session"
-)
-
-#Get the Amazon client role credentials
-s3_client = Aws::S3::Client.new(credentials: role_credentials)
 
 #Different the operation name matches ARGV[1]
 case operation
